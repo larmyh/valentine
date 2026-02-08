de8ce315609a0b286d0119f2ca0beedb1918480b
larmyh <114211726+larmyh@users.noreply.github.com>
2026-02-08 18:23:40 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+const yesButton = document.getElementById('yesButton');
+const noButton = document.getElementById('noButton');
+const question = document.getElementById('question');
+
+let fontSize = 1.2;
+
+noButton.addEventListener('click', () => {
+    // Make the Yes button bigger
+    fontSize += 0.5;
+    yesButton.style.fontSize = `${fontSize}rem`;
+    
+    // Change the "No" text to be more persuasive
+    const phrases = ["Are you sure?", "Really?", "Think again!", "Pwease?", "Don't do this!"];
+    noButton.innerText = phrases[Math.floor(Math.random() * phrases.length)];
+});
+
+yesButton.addEventListener('click', () => {
+    question.innerText = "Yay! See you on the 14th! 🥰";
+    document.querySelector('.buttons').style.display = 'none';
+});
